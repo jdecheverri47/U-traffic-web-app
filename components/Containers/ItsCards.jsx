ea34c84293b322b73f0767d1841079fa6379246c
Jose Echeverri <josecheverri47@gmail.com>
2023-06-22 12:21:07 -0500

    ITS module done, working fine, animations missing

--- components/Containers/ItsCards.jsx
+++ components/Containers/ItsCards.jsx
@@ -1,10 +1,38 @@
 import Image from 'next/image'
 
-
+const ItsData = [
+  {
+    id: 1,
+    description: 'Suministro de equipos',
+    img: '/assets/images/Suministro.png',
+  },
+  {
+    id: 2,
+    description: 'Equipo certificado humano',
+    img: '/assets/images/RecursoHumano.png',
+  },
+  {
+    id: 3,
+    description: 'Suministro de SCADA abierto',
+    img: '/assets/images/Scada.png',
+  }
+]
 
 function ItsCards() {
+  const cards = ItsData.map((card) => (
+    <div key={card.id} className='flex justify-center items-center w-[20rem] h-[20rem] flex-wrap flex-col bg-emerald-300 rounded-br-[3rem] p-4 shadow-md border border-cyan-950 bg-opacity-90'>
+      <div className='flex justify-center items-center'>
+        {card.id === 1 ? <Image src={card.img} width={50} height={50} alt={card.description} /> : <Image src={card.img} width={100} height={100} alt={card.description} style={{marginTop: '1.5rem'}}/> }
+      </div>
+      <div className='flex justify-center items-center h-fit mt-5'>
+        <h1 className='text-cyan-950 text-center font-semibold text-2xl z-30 pointer-events-none align-bottom mt-2'>
+          {card.description}
+        </h1>
+      </div>
+    </div>
+  ))
   return (
-    <div>ItsCards</div>
+    cards
   )
 }
 
--- components/Individuals/ItsSlide.jsx
+++ components/Individuals/ItsSlide.jsx
@@ -1,6 +1,10 @@
 import Image from "next/image";
+import ItsCards from "../Containers/ItsCards";
 
 function ItsSlide({ onCloseSlide }) {
+
+  const classNameH2 = 'text-cyan-950 text-3xl ml-3 text-center'
+  const classContainer = 'flex flex-col justify-center'
 
   const closeSlide = () => {
     console.log(onCloseSlide)
@@ -18,11 +22,39 @@ function ItsSlide({ onCloseSlide }) {
 
         <div className="h-[80vh] w-[90vw] absolute bottom-[11%] left-[5%] bg-white rounded-xl bg-opacity-80 shadow-2xl grid grid-cols-2">
           <div className="relative mt-10 ml-10">
-            <h1 className="text-cyan-950 font-bold text-5xl">ITS</h1>
-            <h2 className="text-cyan-950 text-4xl mt-2">
-              Solución para el cumplimiento con el contrato ITS
-              de los <strong>proyectos en concesión 4G y 5G.</strong>
-            </h2>
+            <div>
+              <h1 className="text-cyan-950 font-bold text-5xl">ITS</h1>
+              <h2 className="text-cyan-950 text-4xl mt-10">
+                Solución para el cumplimiento con el contrato ITS
+                de los <strong>proyectos en concesión 4G y 5G.</strong>
+              </h2>
+            </div>
+            <div className="flex flex-row gap-5 mt-[5rem]">
+              <ItsCards />
+            </div>
+          </div>
+
+          <div className="relative flex justify-center">
+            <div className='absolute bg-teal-600 top-10 p-5 ml-5 rounded-br-[2rem]'>
+              <h1 className='text-white text-3xl uppercase font-bold'>más de 15 años de experiencia</h1>
+            </div>
+
+            <div className="mt-[10rem]">
+              <div className='grid grid-rows-3 mt-6 mx-10 gap-12 justify-center items-center'>
+                <div className={classContainer}>
+                  <h1 className='text-cyan-950 font-bold text-8xl text-center'>35Km</h1>
+                  <h2 className={classNameH2}>De túneles en 36 proyectos </h2>
+                </div>
+                <div className={classContainer}>
+                  <h1 className='text-cyan-950 font-bold text-8xl text-center '>10+</h1>
+                  <h2 className={classNameH2}>Proyectos entregados</h2>
+                </div>
+                <div className={classContainer}>
+                  <h1 className='text-cyan-950 font-bold text-8xl text-center'>500+</h1>
+                  <h2 className={classNameH2}>Km de ITS de cielo abierto</h2>
+                </div>
+              </div>
+            </div>
           </div>
 
         </div>
